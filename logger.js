f7743c7dccf0dba7e8851404df3a6c8947754e04
Oloruntobi Allen <oloruntobi.allen@gmail.com>
2018-04-10 14:10:30 +0100

    Added logger

---
+++ logger.js
@@ -0,0 +1,20 @@
+const bunyan = require('bunyan')
+
+exports.loggerInstance = bunyan.createLogger({
+    name: 'transaction-notifier',
+    serializers: {
+        req: require('bunyan-express-serializer'),
+        res: bunyan.stdSerializers.res,
+        err: bunyan.stdSerializers.err
+    },
+    level: 'info'
+});
+
+exports.logResponse = function (id, body, statusCode) {
+    var log = this.loggerInstance.child({
+        id: id,
+        body: body,
+        statusCode: statusCode
+    }, true)
+    log.info('response')
+}
