4e8e77191818802d5c95769045d0d530587c7dd1
Oloruntobi Allen <oloruntobi.allen@gmail.com>
2018-04-10 12:14:25 +0100

    Added endpoints

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
+
+app.get("/health", function (req, res) {
+    res.status(200).send();
+});
+
+app.post("/stuff", function (req, res) {
+
+    var response = {
+        fullname: `${req.body.firstname} ${lastname}`
+    }
+    res.status(200).send(response);
+});
 
 app.set('port', process.env.PORT || 8081);
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
+    console.log(`Express running → PORT ${server.address().port}`);
 });
